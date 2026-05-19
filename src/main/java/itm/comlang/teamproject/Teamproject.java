78237781a10eb769903da65b05975d9a7c746ef1
Kapeu <Kapeu@10.50.33.16>
2026-05-19 20:51:17 +0900

    First Implement

---
+++ src/main/java/itm/comlang/teamproject/Teamproject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itm.comlang.teamproject;
+
+/**
+ *
+ * @author Kapeu
+ */
+public class Teamproject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
